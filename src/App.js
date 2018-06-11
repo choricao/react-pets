9ac6ddadc53f678ddb8174677cb62d69471d220d
Zheng Cao <chori84@gmail.com>
2018-06-11 10:01:58 -0700

    Add hard coded data PET_LIST

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import Pet from './components/Pet';
 import './App.css';
+
+const PET_LIST = [
+  {
+    name: 'Bogart',
+    age: 8,
+    breed: 'Mixed',
+    about: 'Bogart was Kirsten\'s Great cat!',
+  },
+  {
+    name: 'Atticus',
+    age: 2003,
+    breed: 'Irish Wolfhound',
+    about: 'Atticus is a senior citizen!',
+  },
+  {
+    name: 'Bagel',
+    age: 1,
+    breed: 'Beagle',
+    about: 'Bagel the Beagle',
+  }
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      {/* fill it in!!! */}
+        <Pet
+          name={PET_LIST[0].name}
+          age={PET_LIST[0].age}
+          breed={PET_LIST[0].breed}
+          about={PET_LIST[0].about}
+          />
       </div>
     );
   }
