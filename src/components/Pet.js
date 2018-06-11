e8d25abc45956e7b44285e07dcd7b1a11d2a8b48
Zheng Cao <chori84@gmail.com>
2018-06-11 10:11:53 -0700

    Implement Pet component

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -2,7 +2,24 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Pet extends Component {
-  
+
+  render() {
+    return (
+      <article>
+        <h3>Pet {this.props.name}</h3>
+        <p><strong>Age: </strong> {this.props.age}</p>
+        <p><strong>Breed: </strong> {this.props.breed}</p>
+        <p>{this.props.about}</p>
+      </article>
+    );
+  }
 }
+
+Pet.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired,
+  breed: PropTypes.string.isRequired,
+  about: PropTypes.string.isRequired,
+};
 
 export default Pet;
