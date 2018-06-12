23c34035100a26bc86bb2b7b998392acdfa281b8
Zheng Cao <chori84@gmail.com>
2018-06-12 11:09:07 -0700

    Use shallow rendering for App Snapshot test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { mount, shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App />', () => {
+
+  // test('will match the last snapshot with deep rendering', () => {
+  //   const wrapper = mount(<App />);
+  //   expect(wrapper).toMatchSnapshot();
+  // });
+
+  test('that it renders App with shallow rendering', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+    wrapper.unmount();
+  });
 });
